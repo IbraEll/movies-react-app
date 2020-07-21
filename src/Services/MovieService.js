274cee8a1eca3e0d6276a2b9242a60d54001a2ba
Lina <ibragimowa.ellina@yandex.ru>
2020-07-21 23:51:50 +0300

    Api вынесен в отдельный файл

---
+++ src/Services/MovieService.js
@@ -0,0 +1,29 @@
+export default class MovieService{
+    constructor(){
+        this._apiBase = 'https://api.themoviedb.org/3';
+        this._apiKeyLang = '?api_key=4237669ebd35e8010beee2f55fd45546&language=ru';
+    }
+
+    async getRresource(url){
+        const result = await fetch(`${this._apiBase}${url}${this._apiKeyLang}`);
+
+        if(!result.ok){
+            throw new Error (`Couldn't fetch data from ${url}. Status: ${result.status}`);
+        }
+
+        return await result.json();
+    }
+    getGenres(){
+        return this.getRresource('/genre/movie/list');
+    }
+    getMovies(sorting = "popularity.desc",
+                currentPage = 1,
+                genre = 0){
+        let url  = `/discover/sort_by=${sorting}&page=${currentPage}`;
+        if (genre > 0) url += `&with_genres=${genre}`;
+        return this.getRresource(url);
+    }
+    getMovie(id){
+        return this.getRresource(`/movie/${id}`);
+    }    
+}
